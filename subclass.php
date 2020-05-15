3b891e6d078fb82d1f3305a1ba1d05d0d73d08c3
liubohung <liubohung@gmail.com>
2020-05-15 22:19:45 +0800

    add new in subclass to chech is haveto class or not and notice .

--- subclass.php
+++ subclass.php
@@ -1,3 +1,7 @@
+<input type="button" onmousedown="alert('Hello')" value="測試按鈕">
+
+
+
 <?php
 	require_once("conect.php");
 	session_start();
@@ -5,19 +9,20 @@
 	if ( isset($account) && isset($_POST['subchoose'])) {
 		$code = $_POST['subchoose'] ;
 		try{
-			$check = "SELECT Code FROM class WHERE Person_id = '$account' AND Code ='$code';";
+			$check = "SELECT class.Code,Haveto FROM class JOIN class_detail ON class.Code = class_detail.Code WHERE Person_id = '$account' AND Code ='$code'; ";
 			$sub = "DELETE FROM class WHERE Person_id = '$account' AND Code ='$code';";
 			$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
 			$do_it = $db->query($check);
 			$result = $do_it->fetch(PDO::FETCH_ASSOC);
 			if(!empty($result)){
+				if($result['Haveto'] == 'M'){
+					print<<<_END
+						<script>
+						alert ("退選成功");
+						</script>
+					_END;
+				}
 				$row = $db->exec($sub);
-				print "$row";
-				print<<<_END
-				<script>
-				alert ("退選成功");
-				</script>
-				_END;
 			}else{
 				print<<<_END
 				<script>
