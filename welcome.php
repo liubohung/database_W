69a629ea21fa14cec16837a8984a80363b6e5e2c
liubohung <liubohung@gmail.com>
2020-05-24 20:09:30 +0800

    fix the lost in searchtry.php

--- welcome.php
+++ welcome.php
@@ -73,7 +73,7 @@
 		$rows = $cdata->fetchAll(PDO::FETCH_ASSOC);
 		print "<div style=\"text-align:center;\"><H3> 已選課表 </H3></div><br> ";
 		print " <table style=\"width:80%;\" border=\"1\" align=\"center\">";
-		print "<thead><tr style=\"height:40px\"> <th><div style=\"text-align:center;\">開課班級</div></th> <th><div style=\"text-align:center;\">課程名稱</div></th> <th><div style=\"text-align:center;\">選課代號</div></th><th><div style=\"text-align:center;\">學分數</div></th> <th><div style=\"text-align:center;\">必選修</div></th><th><div style=\"text-align:center;\">開課單位</div></th> <th><div style=\"text-align:center;\">收授人數</div></th> <th><div style=\"text-align:center;\">開課人數</div></th> <th><div style=\"text-align:center;\">授課教師</div></th> </tr></thead>" ;
+		print "<thead><tr style=\"height:40px\"> <th><div style=\"text-align:center;\">開課班級</div></th> <th><div style=\"text-align:center;\">課程名稱</div></th> <th><div style=\"text-align:center;\">選課代號</div></th><th><div style=\"text-align:center;\">學分數</div></th> <th><div style=\"text-align:center;\">必選修</div></th><th><div style=\"text-align:center;\">開課單位</div></th> <th><div style=\"text-align:center;\">收授人數</div></th> <th><div style=\"text-align:center;\">開課人數</div></th> <th><div style=\"text-align:center;\">授課教師</div></th> </tr></thead><tbody>" ;
 		foreach($rows as $row){
 			print "<tr style=\"height:40px\">";
 			foreach($row as $key => $value){
@@ -81,7 +81,7 @@
 			}
 			print " </tr>";
 		}
-		print "</table><br>";
+		print "</tbody></table><br>";
 
 		$credit_T = "SELECT Credit FROM student WHERE Student_id = '$account' ;";
 		$data_t = $db->query($credit_T);
