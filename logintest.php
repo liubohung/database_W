2bb4136bbc72bdfb611fc38071f23feb1d91648d
liubohung <liubohung@gmail.com>
2020-05-28 11:45:52 +0800

    ddd

--- logintest.php
+++ logintest.php
@@ -44,41 +44,7 @@
 		}else if($account_T == 'T') {
 			
 		}
-		try{
-			$db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
-			$cusr=$db->query("SELECT Student_id FROM student WHERE Student_id = '$account';");
-			$row1=$cusr->fetch(PDO::FETCH_BOTH);
-			if(empty($row1[0])){
-				$db=null;
-				print<<<_END
-				<script>
-				alert ("使用者不存在");
-				setTimeout(function(){window.location.href='login.html';},1000);
-				</script>
-				_END;
-			}else {
-				$cpwd=$db->query("SELECT Password FROM student WHERE Student_id = '$account';");
-				$row2=$cpwd->fetch(PDO::FETCH_BOTH);
-				if(!password_verify( $pwd ,$row2[0])){
-					$db=null;
-					print<<<_END
-					<script>
-					alert ("使用者名稱或密碼錯誤");
-					setTimeout(function(){window.location.href='login.html';},1000);
-					</script>
-					_END;
-				}else{
-					$db=null;
-					session_start();
-					$_SESSION['account'] = $account;
-					$_SESSION['pwd'] = $pwd;
-					header("refresh:0;url=welcome.php");
-					exit;
-				}
-			}
-		}catch (PODException $e){
-			print "couldn't to connect to db " . $e->getMessage();
-		}
+			
 	} else {
 		print<<<_END
 				<script>
