1fc8d2f379b1572c61e01deff0d5642ec2d8be9b
liubohung <liubohung@gmail.com>
2020-06-04 10:56:07 +0800

    have to debug DT.php dt.php DTD.php

--- admin/DT.php
+++ admin/DT.php
@@ -1,9 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>show detail</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist\css\bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>     
+    <script src="bootstrap-3.3.7-dist\js/bootstrap.min.js"></script>
+</head>
+
+<script>
+	function post_sub(PARAMS) {        
+    			var temp = document.createElement("form");        
+    			temp.action = "DTD.php";        
+    			temp.method = "post";        
+    			temp.style.display = "none";
+    			var opt = document.createElement("input");        
+        		opt.name = 'T_id';        
+        		opt.value = PARAMS;
+        		temp.appendChild(opt);        
+    			document.body.appendChild(temp);        
+    			temp.submit();        
+    			return temp;        
+	}
+</script>
+<body>
+    <h1>教師資料</H1>
 <?php
     require_once("conect.php");
-    header("Content-type: application/json; charset=utf-8");
     try{
         $db = new PDO('mysql:host=localhost;dbname=class_database',$connect_un,$connect_pw);
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
             if (isset($_POST['search_item'] == "id") && isset($_POST['Teacher_id'])) {
                 $Teacher_id = $_POST['Teacher_id'];
                 $sql_q= "SELECT * FROM teacher WHERE Teacher_id= '$Teacher_id';";
@@ -15,12 +40,84 @@
                 $query = $db->query($q);
                 $datalist = $query->fetch();
             }
-            echo json_encode($datalist,JSON_UNESCAPED_UNICODE);
-        }else {
-            echo json_encode(array('errorMsg' => 'only POST'));
-        }
+            print<<<_table
+                <table>
+                    <tr><td>教師id</td><td>$datalist['Teacher_id']</td></tr>
+                    <tr><td>名字</td><td>$datalist['Name']</td></tr>
+                    <tr><td>學院</td><td>$datalist['College']</td></tr>
+                    <tr><td>系級</td><td>$datalist['Department']</td></tr>
+                    <tr><td>班級</td><td>$datalist['Class']</td></tr>
+                    <tr><td>信箱</td><td>$datalist['Email']</td></tr>
+                    <tr><td>職別</td><td>$datalist['Level']</td></tr>
+                </table>
+                <input name=\"button_del\" value=\"刪除\" onclick =\"post_sub(\"$datalist['Teacher_id']\")\">"
+                 _table;
     }catch(PDOException $execption){
-        echo json_encode(array('errorMsg' => "SQL Connection failed"));
+        echo "SQL Connection failed";
     }
-
 ?>
+<h1>修改資料</h1>
+    <form name="myForm" action="add_teacher_data.php" method="post" >
+		<p>
+		  <tr>
+		    <th>請輸入教師</th>
+		    <th><input type="text" name="Teacher_id"></th>
+		  </tr>
+		</p>
+		<p>
+		  <tr>
+		    <td>請輸入學院</td>
+		  </tr>
+		  <select name="College"  onChange="renew(this.selectedIndex);">
+		  	<option value="">請選擇</option>
+　			<option value="資電學院">資電學院</option>
+　			<option value="商學院">商學院</option>
+　			<option value="通識中心">通識中心</option>
+		  </select>
+		</p>
+		<p>
+		 <tr>
+		  	<td>請輸入系級</td>
+		 </tr>
+			<select name="Department" >
+				<option value="">
+			</select>
+		</p>
+		<p>
+		  <tr>
+		  	<td>請輸入班級</td>
+		  	<td><input type="text"name="Class"></td>
+		  </tr>
+		</p>
+		<p>
+		  <tr>
+		  	<td>請輸入姓名</td>
+		  	<td><input type="text"name="Name"></td>
+		  </tr>
+		</p>
+		<p>
+		  <tr>
+		  	<td>請輸入信箱</td>
+		  	<td><input type="text"name="Email"></td>
+		  </tr>
+		</p>
+		<p>
+		  <tr>
+		  	<td>請輸入密碼</td>
+		  	<td><input type="text"name="Password"></td>
+		  </tr>
+		</p>
+		<p>
+		  <tr>
+		  	<td>請輸入職稱</td>
+		  </tr>
+		  <select name="Level">
+　			<option value="教授">教授</option>
+　			<option value="副教授">副教授</option>
+　			<option value="助理教授">助理教授</option>
+　			<option value="職員">職員</option>
+		  </select>
+		</p>
+		<input type="submit" name="value" value="提交" />
+		<input type="reset" name="value" />
+	</form>
--- admin/delete_teacher.php
+++ admin/delete_teacher.php
@@ -3,69 +3,27 @@
 <head>
 	<title>Delete Student data</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>     
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>  
     <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist\css\bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>     
-    <script src="bootstrap-3.3.7-dist\js/bootstrap.min.js"></script>
+	<script src="bootstrap-3.3.7-dist\js/bootstrap.min.js"></script>
 </head>
 <body>
-<script>
-	function post_sub(PARAMS) {        
-    			var temp = document.createElement("form");        
-    			temp.action = "DTD.php";        
-    			temp.method = "post";        
-    			temp.style.display = "none";
-    			var opt = document.createElement("input");        
-        		opt.name = 'T_id';        
-        		opt.value = PARAMS;
-        		temp.appendChild(opt);        
-    			document.body.appendChild(temp);        
-    			temp.submit();        
-    			return temp;        
-	}
-    $("#button_s").click(function(){
-        $( "#mytbody" ).empty();
-		$( "#mytbody" ).append("<tr id=\"T_id\"></tr><tr id=\"name\"></tr><tr id=\"college\"></tr><tr id=\"department\"></tr><tr id =\"class\"></tr><tr id=\"mail\"></tr><tr id=\"level\"></tr>");
-        $.ajax({
-            type: "POST",
-            url: "DT.php",
-            dataType: "html",
-            data: {
-                Class: $("#member").val(),
-                Collage: $("#department").val(),
-            },
-            error: function(xhr) {
-                alert('Ajax request 發生錯誤');            
-			},
-            success: function(msg){
-                var array_return =  $.parseJSON ( msg );
-				if(array_return['errorMsg'] == "SQL Connection failed" || array_return['errorMsg'] == "only POST"){
-					alert("發生錯誤");
-				}else{
-					var dii = "<td>";
-                    var eddi = "</td>";
-					$("#T_id").append("<td>教師id</td>" +dii + array_return['Teacher_id'] + eddi);
-					$("#name").append("<td>名字</td>" + dii + array_return['Name'] + eddi);
-					$("#college").append("<td>學院</td>"+dii+array_return['College'] + eddi);
-					$("#department").append("<td>系級</td>"+dii+array_return['Department'] + eddi);
-					$("#class").append("<td>班級</td>"+dii+array_return['Class'] + eddi);
-					$("#mail").append("<td>信箱</td>"+dii+array_return['Email'] + eddi);
-					$("#level").append("<td>職別</td>"+dii+array_return['Level'] + eddi);
-					$("mydiv").append("<input name=\"button_del\" value=\"刪除\" onclick =\"post_sub(" + array_return['Teacher_id'] + ")\">");
-				}  
-            }
-        });
-    });
-    </script>
-	<h1>刪除教師資料</h1>
-	<form>
+    <?php
+	    include "admin_nav.php";
+        nav_in();
+    ?>
+	<h1>修改教師資料</h1>
+	<form action="DT.php" method="post">
 		<select name="查詢項目">
 		　<option value="id">教師id</option>
 		　<option value="name">姓名</option>
 		</select>
 		<input type="text" name="Teacher_id">
 		<input type="submit" id="button_s" value="查詢"/>
-	</form>
-	<table id="mydiv" border="1" align="center"><tbody id ="mytbody">
-	</tbody></table>
+    </form>
 </body>
 </html>
